a3b15b49f06188fd850757105abbb013ead3c7ae
AJ Javadi <amirjavadi25@gmail.com>
2024-09-26 15:01:57 -0500

    Initial Commit

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,4 +1,5 @@
-import '../css/app.css';
+// import '../css/app.css';
+import '../sass;/app.scss'; // main SASS stylesheet
 import './bootstrap';
 
 import { createInertiaApp } from '@inertiajs/react';
