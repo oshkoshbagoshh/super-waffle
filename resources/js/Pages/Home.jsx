3c3a51cc2e115918547dfea132036a5ca9321b8e
AJ Javadi <amirjavadi25@gmail.com>
2024-09-26 17:20:41 -0500

    Main Layout

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import MyComponent from '../Components/MyComponent.jsx';
-import MainLayout from './Layouts'
-
+import MainLayout from '../Layouts/MainLayout.jsx'
+import Hero from '@/Components/Hero.jsx';
+import About from '@/Components/About.jsx';
+import Services from '@/Components/Services.jsx';
+import FAQ from '@/Components/FAQ.jsx';
+import ContactForm from '@/Components/ContactForm.jsx';
 
 // function
+export default function Home() {
+    return (
+        <MainLayout>
+            <Hero />
+            <About />
+            <Services />
+            <ContactForm />
+        </MainLayout>
+    );
+}
 
